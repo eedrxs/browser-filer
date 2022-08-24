b0e700ddfaaae37af19e09116835ae5203fc4092
Idris Abdul-Lateef <alabialade@gmail.com>
2022-08-24 19:51:25 +0100

    Added click to file input, forgot to do that before

--- src/utils/types.ts
+++
@@ -1,7 +0,0 @@
-export type FileInput = HTMLInputElement & {
-  type: string;
-  multiple: boolean;
-  style: {
-    display: string;
-  };
-};
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,17 +1,17 @@
-import { FileInput } from './types';
-
-function createFileInput(isMultiple = false) {
-  const fileInput = Object.assign(document.createElement('input'), {
+function createFileInput(isMultiple = false): HTMLInputElement {
+  const fileInput = {
+    ...document.createElement('input'),
     type: 'file',
     multiple: isMultiple,
     style: { display: 'hidden' },
-  });
+  };
 
   document.body.appendChild(fileInput);
-  return fileInput;
+  fileInput.click();
+  return fileInput as HTMLInputElement;
 }
 
-function createFilePromise(fileInput: FileInput): Promise<FileList | null> {
+function createFilePromise(fileInput: HTMLInputElement): Promise<FileList | null> {
   return new Promise((resolve) => {
     fileInput.onchange = (event) => {
       const files = (event.target as HTMLInputElement).files;
